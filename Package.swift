ea11ac6638e7c766ee6a528d4c8316cb37f32bf5
Amr Aboelela <amraboelela@gmail.com>
2021-05-28 14:23:23 -0700

    amrleveldb

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "leveldb",
+    name: "amrleveldb",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "leveldb",
+            name: "amrleveldb",
             targets: ["leveldb"])
     ],
     dependencies: [
