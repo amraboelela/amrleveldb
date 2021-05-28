b249d65310cd6aab1b723dccde059a15c2953b6e
Amr Aboelela <amraboelela@gmail.com>
2021-05-28 14:29:29 -0700

    name: "leveldb",

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "amrleveldb",
+            name: "leveldb",
             targets: ["leveldb"])
     ],
     dependencies: [
